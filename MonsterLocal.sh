3d730f961a5a0842778efd3230764a9650277673
Monster778 <43908973+Monster778@users.noreply.github.com>
2018-10-20 23:30:12 +0300

    Add files via upload

---
+++ MonsterLocal.sh
@@ -0,0 +1,6 @@
+pkg install php
+echo to Run the localhost in you chrom localhost:8080 
+mkdir /sdcard/php/index.html
+php -S localhost:8080 -t /sdcard/php
+echo Done 
+echo now run php -S localhost:8080 -t /sdcard/php
